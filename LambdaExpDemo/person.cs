b1e6ce72e032454a06f242a80aec7f5bfdecf306
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-25 12:26:24 +0530

    [AKSHAY] added lambad exp demo project

---
+++ LambdaExpDemo/person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpDemo
+{
+    class Person
+    {
+        public string SSN;
+        public string Name;
+        public string address;
+        public int Age;
+
+        public Person(string ssn, string name, string addr, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            address = addr;
+            Age = age;
+
+        }
+    }
+}
